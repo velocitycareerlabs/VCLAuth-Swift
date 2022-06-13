c0ccbc350c0a33e805d62e10b399eb4039c78f9e
Michael Avoyan <michael.avoyan.dev@gmail.com>
2022-06-13 14:27:00 +0300

    v0.1.0

---
+++ Frameworks/VCLAuth.xcframework/ios-arm64/VCLAuth.framework/Headers/VCLAuth.h
@@ -0,0 +1,18 @@
+//
+//  VCLAuth.h
+//  VCLAuth
+//
+//  Created by Michael Avoyan on 12/06/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VCLAuth.
+FOUNDATION_EXPORT double VCLAuthVersionNumber;
+
+//! Project version string for VCLAuth.
+FOUNDATION_EXPORT const unsigned char VCLAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VCLAuth/PublicHeader.h>
+
+
